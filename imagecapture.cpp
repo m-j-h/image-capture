1886d86a79fb1e983f0c7ca7667235696d0fcc3e
mark <me@me>
2019-04-09 19:40:41 +0100

    support options

--- imagecapture.cpp
+++ imagecapture.cpp
@@ -6,21 +6,24 @@
 
 int main( int argc, char** argv )
 {
-    if( argc != 2 )
+    if( argc < 2 || argc > 3 )
     {
+        std::cerr << "usage: imagecapture <filename> [<device>]" << std::endl;
         return EXIT_FAILURE;
     }
-    const std::string filePrefix( argv[1] );
-
+    const std::string filename( argv[1] );
+    const std::string device = ( argc == 3 ) ? std::string(argv[2]) : std::string("/dev/video0");
+ 
     try
     {
-        Camera camera( "/dev/video0" );
+        Camera camera( device );
         auto image = camera.CaptureImage();
-        image.Save( "foo.img" );
+        image.Save( filename );
     }
     catch( std::exception& ex )
     {
         std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
